dfeb830379bc22f5eceea1a98eec84c875031a69
Gustavo Barbosa <janioguga@gmail.com>
2020-05-20 21:47:32 -0300

    insercao de head and tail

--- python_datacamp_pandas/ramdomness.py
+++ python_datacamp_pandas/ramdomness.py
@@ -8,6 +8,7 @@ print(np.random.randint(1, 7))
 # rolando os dados
 step = 50
 dice = np.random.randint(1, 7)
+
 if dice <= 2:
     step = step - 1
 elif dice <= 5:
@@ -16,3 +17,14 @@ else:
     step = step + np.random.randint(1, 7)
 print(dice)
 print(step)
+
+#Cara e coroa usando random walk
+np.random.seed(123)
+outcomes = []
+for x in range(10):
+    coin = np.random.randint(0, 2)
+    if coin == 0:
+        outcomes.append('heads')
+    else:
+        outcomes.append('tails')
+    print('Seu resultado é: ', outcomes)
